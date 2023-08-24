8d8948a898c81431ded00c8a740d5af5be78c372
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-08-25 00:33:07 +0400

    feat:add login route view with translations

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('register.index'));
 });
+
+Route::view('register', 'auth.signup.index')->name('register.index');
